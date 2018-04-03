74daa665d0eaad79e3060f7b07309889498eb4d7
MattAmos <matthewjamesamos@gmail.com>
2018-04-03 17:45:40 +1000

    Add logging of confusion matrix and validation calculation and comparison

--- mushroom/mushroom.py
+++ mushroom/mushroom.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt  # matplotlib 	-- plotting
 import yaml						 # yaml 		-- reading/writing config files
 import time						 # time 		-- performance measure
 import csv
-import pandas as pd
 import pickle
 import random
 
@@ -23,7 +22,6 @@ def split_data(x, y, train_ratio=0.8):
 
 # Will load data in form as specified by the dataset description 
 #	(https://archive.ics.uci.edu/ml/machine-learning-databases/mushroom/agaricus-lepiota.names) 
-# Will load data in from the spiral dataset csv as store as x = [(x, y) ...] and y = [(c) ...]
 def load_data(data_file):
 	with open(data_file, 'r') as csvfile:
 		csvreader = csv.reader(csvfile, delimiter=',')
@@ -61,10 +59,12 @@ def construct_network(inp, weights, biases, neurons):
 	fc3 = tf.nn.sigmoid(tf.add((tf.matmul(fc2, weights['fc3'])), biases['fc3']), name='fc3')
 	fc4 = tf.nn.sigmoid(tf.add((tf.matmul(fc3, weights['fc4'])), biases['fc4']), name='fc4')
 	fc5 = tf.nn.sigmoid(tf.add((tf.matmul(fc4, weights['fc5'])), biases['fc5']), name='fc5')
-	fc6 = tf.nn.sigmoid(tf.add((tf.matmul(fc5, weights['fc6'])), biases['fc6']), name='fc6')
+	fc6 = tf.nn.sigmoid(tf.add((tf.matmul(fc4, weights['fc6'])), biases['fc6']), name='fc5')
+	fc7 = tf.nn.sigmoid(tf.add((tf.matmul(fc5, weights['fc7'])), biases['fc7']), name='fc6')
 
-	return fc6
+	return fc7
 
+# Save model weights, biases and neurons per layer to file at <path>
 def save_model(sess, weights, biases, neurons, path):
 	with open(path, "wb") as pf:
 		# Create new dictionary containers for non-tf types
@@ -77,24 +77,27 @@ def save_model(sess, weights, biases, neurons, path):
 		pickle.dump(model, pf)
 		return model
 
+# Load model weights, biases and neurons per layer to file from <path>
 def load_model(path):
 	with open(path, "rb") as pf:
 		model = pickle.load(pf)
 		return model
 
 def train_network(sess, x, y, cfg):
+	t_cfg = cfg['nn']
+
 	# Alias config vars
-	neurons       = cfg['training']['nn']['neurons']
-	epochs        = cfg['training']['nn']['epochs']
-	learning_rate = cfg['training']['nn']['learning_rate']
-	iterations    = cfg['training']['nn']['iterations']
-	acc_thresh    = cfg['training']['nn']['accuracy_threshold']
-	model_dir     = cfg['training']['nn']['model_dir']
+	neurons       = t_cfg['parameters']['neurons']
+	epochs        = t_cfg['parameters']['epochs']
+	learning_rate = t_cfg['parameters']['learning_rate']
+	err_thresh    = t_cfg['error_threshold']
+	model_dir     = t_cfg['model_dir']
+	avg_factor    = t_cfg['avg_factor']
 
 	print('[ANN] \tTraining parameters: epochs={0}, learning_rate={1:.2f}, neurons={2}'.format(epochs, learning_rate, neurons))
 
 	# Create validation set
-	x_train, x_valid, y_train, y_valid = split_data(x, y, 0.9)
+	x_train, x_valid, y_train, y_valid = split_data(x, y, 0.95)
 	x_valid, y_valid = shuffle_data(x_valid, y_valid)
 
 	# Create placeholders for tensors
@@ -102,12 +105,13 @@ def train_network(sess, x, y, cfg):
 	y_ = tf.placeholder(tf.float32, [None, 1],  name='y_placeholder')
 
 	weights = {
-		'fc1' : tf.Variable(tf.random_normal([22, neurons]), name='w_fc1'),
+		'fc1' : tf.Variable(tf.random_normal([22, neurons]),      name='w_fc1'),
 		'fc2' : tf.Variable(tf.random_normal([neurons, neurons]), name='w_fc2'),
 		'fc3' : tf.Variable(tf.random_normal([neurons, neurons]), name='w_fc3'),
 		'fc4' : tf.Variable(tf.random_normal([neurons, neurons]), name='w_fc4'),
 		'fc5' : tf.Variable(tf.random_normal([neurons, neurons]), name='w_fc5'),
-		'fc6' : tf.Variable(tf.random_normal([neurons, 1]), name='w_fc6'),
+		'fc6' : tf.Variable(tf.random_normal([neurons, neurons]), name='w_fc6'),
+		'fc7' : tf.Variable(tf.random_normal([neurons, 1]),       name='w_fc7'),
 	}
 
 	biases = {
@@ -116,9 +120,11 @@ def train_network(sess, x, y, cfg):
 		'fc3' : tf.Variable(tf.random_normal([neurons]), name='b_fc3'),
 		'fc4' : tf.Variable(tf.random_normal([neurons]), name='b_fc4'),
 		'fc5' : tf.Variable(tf.random_normal([neurons]), name='b_fc5'),
-		'fc6' : tf.Variable(tf.random_normal([1]), name='b_fc6'),
+		'fc6' : tf.Variable(tf.random_normal([neurons]), name='b_fc6'),
+		'fc7' : tf.Variable(tf.random_normal([1]),       name='b_fc7'),
 	}
 
+	# Construct our network and return the last layer to output the result
 	final_layer = construct_network(x_, weights, biases, neurons)
 
 	# Define error function
@@ -127,15 +133,15 @@ def train_network(sess, x, y, cfg):
 	# Define optimiser and minimise error function task
 	optimiser = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(cost)
 
-	# Create error logging storage
-	train_errors = []
-	valid_errors = []
-
 	# Initialise global variables of the session
 	sess.run(tf.global_variables_initializer())
 
 	# Save model to file
 	model = save_model(sess, weights, biases, neurons, model_dir)
+
+	# Create error logging storage
+	train_errors = []
+	valid_errors = []
 
 	# Setup our continous plot
 	plt.title('Error vs Epoch')
@@ -151,27 +157,61 @@ def train_network(sess, x, y, cfg):
 	# Measure training time
 	t_start = time.time()
 
+	diff_err = 1.
+	vel_err  = 0. 
+	acc_err  = 0.
+
 	for i in range(epochs):
 		_, train_error = sess.run([optimiser, cost], feed_dict={x_: x_train, y_: y_train})
 		_, valid_error = sess.run([optimiser, cost], feed_dict={x_: x_valid, y_: y_valid})
-		print('{0:.4f}'.format(valid_error - train_error))
+		
+		# Add new errors to list
 		train_errors.append(train_error)
 		valid_errors.append(valid_error)
-		if train_error <= 1 - acc_thresh:
+
+		# If we have at least an averageable amount of samples
+		if i > avg_factor:
+			avg_train_error = 0; avg_valid_error = 0
+			# Get sum over last n epochs
+			for j in range(0, avg_factor):
+				avg_train_error += train_errors[i - j]
+				avg_valid_error += valid_errors[i - j]
+			# Average them
+			avg_train_error /= avg_factor
+			avg_valid_error /= avg_factor
+
+			# Calculate change in velocity of error difference
+			acc_err = vel_err - (diff_err - abs(avg_valid_error - avg_train_error))
+
+			# Calculate change in error difference (positive -> convergence, negative -> divergence)
+			vel_err = diff_err - abs(avg_valid_error - avg_train_error)
+			
+			# Calculate error difference between validation and training
+			diff_err = abs(avg_valid_error - avg_train_error)
+			# print('[ANN] Epoch: {0:4d}, Δerr = {1:7.4f}, 𝛿(Δerr) = {2:7.4f}, 𝛿(𝛿(Δerr)) = {3:7.4f}'.format(i, diff_err, vel_err, acc_err)) # DEBUG
+
+		# If we already have our target error, terminate early
+		if train_error <= err_thresh:
 			break
 		
 		# Set plot settings
-		plt.plot(train_errors[:epochs], color='r', label='training')
-		plt.plot(valid_errors[:epochs], color='b', label='validation')
-		plt.draw()
-		plt.pause(0.001)			
+		if i > 0:
+			plt.plot(train_errors[:epochs], color='r', label='training')
+			plt.plot(valid_errors[:epochs], color='b', label='validation')
+			plt.axis([0, i, 0., 1.])
+			plt.draw()
+			plt.pause(0.001)			
 
 	t_elapsed = time.time() - t_start
 
-	# Calculate new accuracy
-	accuracy = 1 - error
+	# Calculate new accuracy from final error
+	accuracy = 1 - train_error
 
-	print('[ANN] \tModel accuracy: {0:.3f}%, Time elapsed: {1:.2f}s'.format(accuracy*100, t_elapsed))
+	print('[ANN] Training Completed:')
+	t_m  = t_elapsed / 60
+	t_s  = t_elapsed % 60
+	t_ms = (t_s % 1) * 1000
+	print('[ANN]\tSimple model accuracy: {0:.3f}%, Time elapsed: {1:2d}m {2:2d}s {3:3d}ms'.format(accuracy*100, int(t_m), int(t_s), int(t_ms)))
 	return model
 
 def test_network(sess, model, x_test, y_test, cfg):
@@ -189,13 +229,14 @@ def test_network(sess, model, x_test, y_test, cfg):
 	# Average out the test timing
 	t_avg_test = (time.time() - t_test) / float(len(y_test))
 
-	print('[ANN] Average time to test: {0:.2f}us'.format(1000000 * t_avg_test))
+	print('[ANN] Testing Completed:')
+	print('[ANN]\tAverage time to test: {0:.2f}us'.format(1000000 * t_avg_test))
 	return result
 
 def analyse_results(y_test, results):
 	# Make variables for true pos, true neg, false pos, false neg
 	tp = 0; tn = 0; fp = 0; fn = 0
-	for i in range(0,len(results)):
+	for i in range(0, len(results)):
 		if y_test[i] == results[i]:	# True
 			if y_test[i][0] == 1.:		# Positive
 				tp += 1
@@ -207,23 +248,41 @@ def analyse_results(y_test, results):
 			else:
 				fn += 1					# Negative
 
-	# Calculate confusion matrix
-	tpr = tp / (tp + fn)					# True Positive Rate
-	tnr = tn / (tn + fp)					# True Negative Rate
-	ppv = tp / (tp + fp)					# Sensitivity
-	npv = tn / (tn + fn)					# Specificity
-	acc = (tp + tn) / (tp + tn + fp + fn)	# Accuracy
+	if tp + fn == 0 or tn + fp == 0 or tp + fp == 0 or tn + fn == 0:
+		print('ERR: Cannot calculate confusion matrix')
+		return None
+	else:
+		# Calculate confusion matrix
+		tpr = tp / (tp + fn)					# True Positive Rate
+		tnr = tn / (tn + fp)					# True Negative Rate
+		ppv = tp / (tp + fp)					# Sensitivity
+		npv = tn / (tn + fn)					# Specificity
+		acc = (tp + tn) / (tp + tn + fp + fn)	# Accuracy
+		certainty = tf.reduce_mean(tf.abs(tf.subtract(results[:, 0], results[:, 0])))
+		print('[ANN] Testing results: ')
+		print('[ANN]\tTrue Positive Rate (TPR): {0:.2f}'.format(tpr))
+		print('[ANN]\tTrue Negative Rate (TNR): {0:.2f}'.format(tnr))
+		print('[ANN]\tSensitivity:              {0:.2f}'.format(ppv))
+		print('[ANN]\tSpecificity:              {0:.2f}'.format(npv))
+		print('[ANN]\tAccuracy                  {0:.2f}'.format(acc))
+		return {
+			'tpr' : tpr,
+			'tnr' : tnr,
+			'ppv' : ppv,
+			'npv' : npv,
+			'acc' : acc
+		}
 
-	certainty = tf.reduce_mean(tf.abs(tf.subtract(results[:, 0], results[:, 0])))
 
-	print('[ANN] Testing results: ')
-	print('[ANN]\tTrue Positive Rate (TPR): {0:.2f}'.format(tpr))
-	print('[ANN]\tTrue Negative Rate (TNR): {0:.2f}'.format(tnr))
-	print('[ANN]\tSensitivity:              {0:.2f}'.format(ppv))
-	print('[ANN]\tSpecificity:              {0:.2f}'.format(npv))
-	print('[ANN]\tAccuracy                  {0:.2f}'.format(acc))
-	print('[ANN]\tCertainty                 {0:.2f}'.format(certainty))
+	# print('[ANN]\tCertainty                 {0:.2f}'.format(certainty))
 
+
+def store_results(conf_mat, conf_file):
+	with open(conf_file, 'w') as yml:
+		yaml.dump(conf_mat, yml)
+		return
+	print('[ERR] Failed to load config file \'{0}\''.format(conf_file))
+	exit()
 ## Main Program
 # Read config file
 cfg  = read_config()
@@ -234,7 +293,7 @@ x_train, y_train = shuffle_data(x_train, y_train)
 x_test, y_test   = shuffle_data(x_test, y_test)
 
 with tf.Session() as sess:
-	if cfg['training']['nn']['train']:
+	if cfg['nn']['train']:
 		# Train network on our training data
 		print('[ANN] Training network...')
 		model = train_network(sess, x_train, y_train, cfg)
@@ -244,4 +303,5 @@ with tf.Session() as sess:
 
 	# Test network on our testing data
 	results = test_network(sess, model, x_test, y_test, cfg)
-	analyse_results(y_test, results)
+	conf_mat = analyse_results(y_test, results)
+	store_results(conf_mat, cfg['nn']['conf_mat_dir'])
